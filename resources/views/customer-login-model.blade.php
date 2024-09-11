8b0a1a7f1d1c5b8b79e793601e2b621902a6b7d7
Yuvaraj <n.yuvarajescet@gmail.com>
2024-09-11 11:17:43 +0530

    otp max length

--- resources/views/customer-login-model.blade.php
+++ resources/views/customer-login-model.blade.php
@@ -51,9 +51,10 @@
                         {{ $otp_sent }}
                         @if ($otp_sent)
                             <div class="relative z-0 w-full mb-5 group">
-                                <input wire:model='otp_receive' type="password"
+                                <input wire:model='otp_receive' type="password" maxlength="6"
                                     class="block py-2.5 px-0 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                                    placeholder="" autocomplete="new-password" inputmode="numeric" />
+                                    placeholder="" autocomplete="new-password" inputmode="numeric"
+                                    oninput="this.value = this.value.replace(/[^0-9]/g, '');" />
                                 <label for="floating_repeat_password"
                                     class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-gray-600 peer-focus:dark:text-gray-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">OTP
                                 </label>
